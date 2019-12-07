e84b5146630b683e425f5bb58ae351350990a77a
iPraveenParihar <praveenparihar687@gmail.com>
2019-12-07 11:03:36 +0530

    Fixed Errors

--- ThreeNodePointToPointNetwork/main.tcl
+++ ThreeNodePointToPointNetwork/main.tcl
@@ -16,10 +16,14 @@ proc finish {} {
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
+set n3 [$ns node]
 
-$ns duplex-link $n0 $n1 200Mb 10ms DropTail
-$ns duplex-link $n1 $n2 1Mb 1000ms DropTail
+$ns duplex-link $n0 $n2 200Mb 10ms DropTail
+$ns duplex-link $n2 $n1 100Mb 5ms DropTail
+$ns duplex-link $n2 $n3 1Mb 1000ms DropTail
+
 $ns queue-limit $n0 $n2 10
+$ns queue-limit $n1 $n2 10
 
 set udp0 [new Agent/Udp]
 $ns attach-agent $n0 $udp0
@@ -28,11 +32,24 @@ $cbr0 set packetSize_ 500
 $cbr0 set interval_ 0.005
 $cbr0 attach-agent $udp0
 
+set upd1 [new Agent/UDP]
+$ns attach-agent $n1 $udp1
+set cbr1 [new Application/Traffic/CBR]
+$cbr1 attach-agent $udp1
+
+set upd2 [new Agent/UDP]
+$ns attach-agent $n2 $udp2
+set cbr2 [new Application/Traffic/CBR]
+$cbr2 attach-agent $udp2
+
 set null0 [new Agent/Null]
-$ns attach-agent $n2 $null0
+$ns attach-agent $n3 $null0
+
 $ns connect $udp0 $null0
+$ns connect $udp1 $null0
 
 $ns at 0.1 "$cbr0 start"
+$ns at 0.2 "$cbr1 start"
 $ns at 1.0 "finish"
 $ns run
 
